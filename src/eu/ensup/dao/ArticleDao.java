b1d542b7b80b6c21c7e0232c32748da506224dad
DaviJam <daori971@gmail.com>
2022-01-10 12:26:38 +0100

    update dao

--- src/eu/ensup/dao/ArticleDao.java
+++ src/eu/ensup/dao/ArticleDao.java
@@ -5,19 +5,19 @@ import eu.ensup.domaine.Article;
 public class ArticleDao {
     public ArticleDao() {
     }
-    Integer create(Article article){
+    public Integer create(Article article){
         System.out.println("New article "+article.getReference()+" added to database.");
         return 0;
     }
-    Article get(String reference){
+    public Article get(String reference){
         System.out.println("Article "+reference+" retrieved from database.");
         return new Article("Olive", 10, 3.5f, "Sec", "ref-sec-002");
     }
-    Integer update(Article article){
+    public Integer update(Article article){
         System.out.println("Article "+article.getReference()+" updated.");
         return 0;
     }
-    Integer delete(String reference){
+    public Integer delete(String reference){
         System.out.println("Article "+reference+" deleted from database.");
         return 0;
     }
